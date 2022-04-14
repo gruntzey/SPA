90ac3431f6fdb8e2243d1aba207276c3db1110bd
gruntzey <gruntzey@yandex.ru>
2022-04-14 06:10:58 +0300

    0.0.1

---
+++ assets/javascript/handlingHeaderItems.js
@@ -0,0 +1,129 @@
+function initHeadHandle() {
+  // function zone
+  function deleteItems() {
+    const range = getRangeBetweenArrowAndBell()
+    if (range < 10 && numOfRemainingElements) {
+      const itemToRemove = document.getElementsByClassName("header__menu-item")[lastItemToHideIndex]
+      const clonedElement = itemToRemove.cloneNode(true)
+      moveItemToSubMenu(true, clonedElement)
+      itemToRemove.remove()
+      setallMenuItemsCollection(menu, subMenu) 
+
+      lastItemToHideIndex--
+      numOfRemainingElements--
+      numOfHiddenElements++
+      deleteItems()
+    }
+    if (range >= 200 && numOfHiddenElements !== 0) {
+      moveItemToSubMenu(false)
+      setallMenuItemsCollection(menu, subMenu) 
+
+      lastItemToHideIndex++
+      numOfRemainingElements++
+      numOfHiddenElements--
+      deleteItems()
+    }
+    if (window.innerWidth < 860) {
+      deleteHoverEffectOnSubListButton()
+    }
+    function deleteHoverEffectOnSubListButton() {
+      arrow.parentElement.classList.remove('hover')
+    }
+  }
+  function getRangeBetweenArrowAndBell() {
+    if (window.innerWidth > 860) {
+      const bellXOffset = bell.getBoundingClientRect().left
+      const arrowXOffset = arrow.getBoundingClientRect().left + arrow.getBoundingClientRect().width
+      return bellXOffset - arrowXOffset
+    }
+    return 1000
+  }
+  function moveItemToSubMenu(add, node) {
+    if (add) {
+      node.classList.remove("header__menu-item")
+      node.classList.add("header__submenu-item")
+      subMenu.insertAdjacentElement('afterbegin', node)
+    } else if (!add) {
+      const clone = subMenu.firstElementChild.cloneNode(true)
+      clone.classList.add("header__menu-item")
+      clone.classList.remove("header__submenu-item")
+      menu.lastElementChild.insertAdjacentElement('beforebegin', clone)
+      subMenu.firstElementChild.remove()
+    }
+  }
+  function setallMenuItemsCollection(menu, subMenu) {
+    allMenuItems = []
+    for (let i = 0; i < menu.children.length - 1; i++) {
+      const element = menu.children[i];
+      if (!element.classList.contains('hidden')) {
+        allMenuItems.push(element)
+      }
+    }
+    for (let i = 0; i < subMenu.children.length - 1; i++) {
+      const element = subMenu.children[i];
+      if (!element.classList.contains('hidden')) {
+        allMenuItems.push(element)
+      }
+    }
+  }
+  function resetActiveElement() {
+    for (let i = 0; i < allMenuItems.length; i++) {
+      const item = allMenuItems[i];
+      if (item.classList.contains('active-item')) {
+        item.classList.remove('active-item')
+        return
+      }
+    }
+  }
+  //
+  // function zone
+  const [favourite] = document.getElementsByClassName("header__favourite")
+  const [bell] = document.getElementsByClassName("header__bell")
+  const arrow = document.getElementById("header__drop-arrow")
+  const [subMenu] = document.getElementsByClassName("header__submenu-items")
+  const [menu] = document.getElementsByClassName("header__menu-items")
+  const [burger] = document.getElementsByClassName("header__menu-burger")
+  let allMenuItems = []
+  let numOfHiddenElements = 0
+  let numOfRemainingElements = document.getElementsByClassName("header__menu-item").length - 1
+  let lastItemToHideIndex = numOfRemainingElements - 1
+
+  deleteItems()
+  setallMenuItemsCollection(menu, subMenu) 
+  window.addEventListener('resize', deleteItems)
+  header.addEventListener('click', function headerClick(e) {
+    //handle bell and favurite
+    if (e.target == favourite || e.target == bell) {
+      e.target.classList.toggle('active-item')
+    }
+
+    //handle active menu item
+    if ((e.target.parentElement == menu && !e.target.classList.contains('header__more-button')) ||
+    e.target.parentElement == subMenu) {
+      
+      resetActiveElement()
+      e.target.classList.add('active-item')
+
+      burger.classList.toggle('opened-burger')
+      menu.classList.toggle('opened-menu')
+    }
+
+    //handle burger
+    if (e.target == burger) {
+      burger.classList.toggle('opened-burger')
+      menu.classList.toggle('opened-menu')
+    }
+
+    if (e.target == arrow.parentElement) {
+      arrow.parentElement.classList.toggle('opened-sub-list')
+    }
+  })
+}
+
+const [header] = document.getElementsByTagName("header")
+header.classList.add('cloaked')
+
+window.addEventListener('load', () => {
+  initHeadHandle()
+  header.classList.remove('cloaked')
+})
